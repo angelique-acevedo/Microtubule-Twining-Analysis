304ea58a1bb4e7a27c623eabace737c141ae727d
Angelique Acevedo <liqacevedo@gmail.com>
2025-05-30 01:28:37 -0500

    Create Supp_1.R
    ###Supplemental Figure 1
    ## Description: This script was used to generate line and boxplots in supplemental figure 1 of (Acevedo et al., in prep), demonstrating the progression of internode elongation across stages (code was made with assistance from Dr. Joyce Onyenedum).

--- Supp_1.R
+++ Supp_1.R
@@ -7,9 +7,8 @@
 library(ggplot2)
 library(rstatix)
 library(ggpubr)
-library (ddply) 
 
-datum <-  read.csv("~/Downloads/Supp_1_Data.csv", stringsAsFactors=TRUE) 
+datum <-  read.csv("~/Downloads/Supp_1_Data_pt1.csv", stringsAsFactors=TRUE) 
 
 #2. Arrange the order in which $stages and $section will appear on the graph and omit all NAs from dataset
 datum$Stage <- factor(datum$Stage,levels = c("S2", "S3", "S5"))
@@ -44,10 +43,10 @@ p<- ggplot(df3, aes(x=Stage, y= Height, colour=Section, group=Section))+
   xlab("Stage") +
   ylab("Internode length (cm)") +
   ggtitle("Internode Development through Stages")  + theme(plot.title = element_text(size = 7), 
-                                            axis.text.x = element_text(size= 7), 
-                                            axis.text.y = element_text(size = 7,), 
-                                            axis.title.x = element_text(size = 7),  
-                                            axis.title.y= element_text(size = 7), legend.position = "top") + theme_bw(base_size = 18)+
+                                                           axis.text.x = element_text(size= 7), 
+                                                           axis.text.y = element_text(size = 7,), 
+                                                           axis.title.x = element_text(size = 7),  
+                                                           axis.title.y= element_text(size = 7), legend.position = "top") + theme_bw(base_size = 18)+
   theme_bw(base_size = 20) +
   scale_color_manual(values = c("#225EA8", "#FE9929", "#CE1256", "#88419D"))+
   ylim(0, 15) 
@@ -60,8 +59,10 @@ library(rstatix)
 library(ggpubr)
 
 #import data
-datum <-  read.csv("~/Downloads/Supp_1_Data.csv", stringsAsFactors=TRUE) 
-length(which(datum$Stage == "S2"))
+datum <-  read.csv("~/Downloads/Supp_1_Data_pt2.csv", stringsAsFactors=TRUE) 
+datum<- na.omit (datum)
+
+datum <- datum %>% filter(Stage == "S2")
 
 #2. Arrange the order in which $section will appear on the graph 
 datum$Section <- factor(datum$Section,levels = c("Hyp", "Epi","IN1", "IN2", "IN3", "IN4", "IN5", "IN6", "IN7", "IN8", "IN9"))
@@ -85,9 +86,9 @@ pwc <- pwc %>% add_xy_position(x = "Group")
 #4. Generates boxplots with data points jittered for each stage and selected sample 
 ggboxplot(datum, x = "Section", y = "Height",
           title= "Length_of_Internodes(cm)", add = "jitter", fill= "Section") +
- #stat_pvalue_manual(pwc, hide.ns = TRUE, y.position = 12) +
+  #stat_pvalue_manual(pwc, hide.ns = TRUE, y.position = 12) +
   #labs(subtitle = get_test_label(res.kruskal, detailed = TRUE),
-   # caption = get_pwc_label(pwc)) +
+  # caption = get_pwc_label(pwc)) +
   theme_bw(base_size = 20) +
   ylim(0,5)+
   ggtitle("Length of Internodes") + 
@@ -98,4 +99,3 @@ ggboxplot(datum, x = "Section", y = "Height",
          axis.text.y = element_text(size = 16), 
          axis.title.x = element_text(size = 16),  
          axis.title.y= element_text(size = 16))
-
