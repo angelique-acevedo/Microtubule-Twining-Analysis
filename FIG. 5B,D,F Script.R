b15eaa2a40ec7f0bed7b76e7fa24e97a654bae61
Angelique Acevedo <liqacevedo@gmail.com>
2025-09-02 07:00:19 -0500

    Create FIG. 5B,D,F Script.R
    ###FIGURE 5B, 5D, 5F 
    ##DESCRIPTION: This script was used to generate boxplots in fig. 5b, 5d, 5f of (Acevedo et al., in prep) of cell area, circularity, and geodesic diameter between the lobes and furrows of selected samples.

--- FIG. 5B,D,F Script.R
+++ FIG. 5B,D,F Script.R
@@ -1,94 +1,122 @@
 ###FIGURE 5B, 5D, 5F 
 ##DESCRIPTION: This script was used to generate boxplots in fig. 5b, 5d, 5f of (Acevedo et al., in prep) of cell area, circularity, and geodesic diameter between the lobes and furrows of selected samples. 
 
-#Load Packages
+# Load Packages
 library(ggplot2)
 library(ggsignif)
 library(dplyr)
-library(cowplot)
+#library(cowplot)
 library(ggpubr)
 
-#1 Load dataset
-data <- read.csv("~/Downloads/FIG_5_Data.csv", stringsAsFactors=TRUE)
+# 1. Load dataset
+data <- read.csv("~/Downloads/FIG_5_Data.csv", stringsAsFactors = TRUE)
 
-#2. Calculate IQR and threshold ranges for each category (area, circularity, and geodesic diameter)
-#AREA
-Q1 <- quantile(data$Area, 0.25)
-Q3 <- quantile(data$Area, 0.75)
-IQR <- Q3 - Q1
+#Filter out objects that got segmented as 'cells'. Area of <100 and GD <10 are too small to have been cells
+data <- data %>%
+  group_by(ID) %>%
+  filter(Area > 100) %>%
+  filter(GedesicDiameter > 10)
 
-lower <- Q1 - 1.5 * IQR
-upper <- Q3 + 1.5 * IQR
+# 1.5 Test dataset for normality 
+##AREA
+normality_results <- data %>%
+  group_by(ID, Lobe_Furrow) %>%
+  summarise(
+    shapiro_p = shapiro.test(Area)$p.value,
+    .groups = "drop"
+  )
+print(normality_results) #S5_IN1 Furrow is normal (pvalue = 7.64x10e-01) but the rest are not!
 
-#CIRCULARITY
-Q11 <- quantile(data$Circularity, 0.25)
-Q33 <- quantile(data$Circularity, 0.75)
-IQR1 <- Q33 - Q11
+##CIRCULARTIY
+normality_results1 <- data %>%
+  group_by(ID, Lobe_Furrow) %>%
+  summarise(
+    shapiro_p = shapiro.test(Circularity)$p.value,
+    .groups = "drop"
+  )
+print(normality_results1)
 
-lower1 <- Q11 - 1.5 * IQR1
-upper1 <- Q33 + 1.5 * IQR1
+##GEODESIC DIAMETER
+normality_results2 <- data %>%
+  group_by(ID, Lobe_Furrow) %>%
+  summarise(
+    shapiro_p = shapiro.test(GedesicDiameter)$p.value,
+    .groups = "drop"
+  )
+print(normality_results2)
+##Due to the variability of normality and non-normalcy, we've decided to select a nonparametric test such as wilcoxon testing
 
-#GEODESIC DIAMETER
-Q111 <- quantile(data$GedesicDiameter, 0.25)
-Q333 <- quantile(data$GedesicDiameter, 0.75)
-IQR2 <- Q333 - Q111
+# 2. Function to remove outliers per facet (ID × Lobe_Furrow)
+remove_outliers <- function(df, value_col) {
+  df %>%
+    group_by(ID, Lobe_Furrow) %>%
+    mutate(
+      Q1 = quantile(.data[[value_col]], 0.25, na.rm = TRUE),
+      Q3 = quantile(.data[[value_col]], 0.75, na.rm = TRUE),
+      IQR = Q3 - Q1,
+      lower = Q1 - 1.5 * IQR,
+      upper = Q3 + 1.5 * IQR
+    ) %>%
+    ungroup() %>%
+    filter(.data[[value_col]] >= lower & .data[[value_col]] <= upper) %>%
+    select(-Q1, -Q3, -IQR, -lower, -upper)
+}
 
-lower11 <- Q111 - 1.5 * IQR2
-upper11 <- Q333 + 1.5 * IQR2
 
-#3. Filter outliers from large datasets based on IQR thresholds
-data_filtered <- data %>% filter(Area > lower & Area < upper)
-data_filtered1 <- data %>% filter(Circularity > lower1 & Circularity < upper1)
-data_filtered11 <- data %>% filter(GedesicDiameter > lower11 & GedesicDiameter < upper11)
+# 3. Apply per variable
+data_area <- remove_outliers(data, "Area")
+data_circ <- remove_outliers(data, "Circularity")
+data_geo  <- remove_outliers(data, "GedesicDiameter")
 
-#4. Complete visualization of boxplots
-##AREA BOXPLOT
-ggplot(data_filtered, aes(x = Lobe_Furrow, y = Area, fill = Lobe_Furrow)) +
+# 4. Visualizations
+## AREA BOXPLOT
+ggplot(data_area, aes(x = Lobe_Furrow, y = Area, fill = Lobe_Furrow)) +
   geom_boxplot() +
-  geom_jitter(alpha=.3)+
-  labs(title = "Area Internode 1", x = "Furrow or Lobe", y = "Area", at=1) +
-  scale_y_continuous(limits = c(0, 3600), breaks = seq (0, 3600, by = 300))+
-  scale_fill_manual(values=c("#43884E","#E8F2E1", "#AAAAAA"))+
-  theme_classic()+
-  facet_wrap(~ID, scales="free")+
-  geom_signif(comparisons = list(c("Furrow", "Lobe")), test="wilcox.test", map_signif_level=TRUE)+
-     stat_compare_means(aes(group=Lobe_Furrow), 
-                     method = "wilcox.test", 
-                     label = "p.format", 
-                     hide.ns = TRUE)+
-  theme_classic()+
-  theme(text = element_text(size=22))
+  geom_jitter(alpha=.3) +
+  labs(title = "Area Internode 1", x = "Furrow or Lobe", y = "Area") +
+  scale_y_continuous(limits = c(0, 3600), breaks = seq(0, 3600, by = 300)) +
+  scale_fill_manual(values=c("#43884E","#E8F2E1", "#AAAAAA")) +
+  facet_wrap(~ID, scales="free") +
+  stat_compare_means(
+    comparisons = list(c("Furrow", "Lobe")),
+    method = "wilcox.test",
+    label = "p.format",
+    p.adjust.method = "bonferroni"
+  )+ 
+  theme_classic(base_size=22)
 
-#CIRCULARITY BOXPLOT
-ggplot(data_filtered1, aes(x = Lobe_Furrow, y = Circularity, fill = Lobe_Furrow)) +
+## CIRCULARITY BOXPLOT
+ggplot(data_circ, aes(x = Lobe_Furrow, y = Circularity, fill = Lobe_Furrow)) +
   geom_boxplot() +
-  geom_jitter(alpha=.3)+
-  labs(title = "Circularity Internode 1", x = "Furrow or Lobe", y = "Circularity", at=1) +
-  scale_y_continuous(limits = c(0, 1), breaks = seq (0, 1, by = .1))+
-   scale_fill_manual(values=c("#43884E","#E8F2E1", "#AAAAAA"))+
-  theme_classic()+
-  facet_wrap(~ID, scales="free")+
-  geom_signif(comparisons = list(c("Furrow", "Lobe")), test="wilcox.test", map_signif_level=TRUE)+
-      stat_compare_means(aes(group=Lobe_Furrow), 
-                     method = "wilcox.test", 
-                     label = "p.format", 
-                     hide.ns = TRUE)+
-  theme_classic()+
-  theme(text = element_text(size=22))
+  geom_jitter(alpha=.3) +
+  labs(title = "Circularity", x = "Furrow or Lobe", y = "Circularity") +
+  scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, by = .1)) +
+  scale_fill_manual(values=c("#43884E","#E8F2E1", "#AAAAAA")) +
+  facet_wrap(~ID, scales="free") +
+  stat_compare_means(
+    comparisons = list(c("Furrow", "Lobe")),
+    method = "wilcox.test",
+    label = "p.format",
+    p.adjust.method = "bonferroni"
+  )+ 
+  theme_classic(base_size=22)
 
-#GEODESIC DIAMETER BOXPLOT
-ggplot(data_filtered11, aes(x = Lobe_Furrow, y = GedesicDiameter, fill = Lobe_Furrow)) +
+## GEODESIC DIAMETER BOXPLOT
+ggplot(data_geo, aes(x = Lobe_Furrow, y = GedesicDiameter, fill = Lobe_Furrow)) +
   geom_boxplot() +
-  geom_jitter(alpha=.3)+
-  labs(title = "Geodesic Diameter Internode 1", x = "Furrow or Lobe", y = "Geodesic Diameter", at=1) +
-  scale_y_continuous(limits = c(0, 160), breaks = seq (0, 160, by = 10))+
-   scale_fill_manual(values=c("#43884E","#E8F2E1", "#AAAAAA"))+
-  theme_classic()+ 
-  facet_wrap(~ID, scales="free")+
-  geom_signif(comparisons = list(c("Furrow", "Lobe")),test="wilcox.test", map_signif_level=TRUE)+
-     stat_compare_means(aes(group=Lobe_Furrow), 
-                     method = "wilcox.test", 
-                     label = "p.format", 
+  geom_jitter(alpha=.3) +
+  labs(title = "Geodesic Diameter", x = "Furrow or Lobe", y = "Geodesic Diameter") +
+  scale_y_continuous(limits = c(0, 200), breaks = seq(0, 200, by = 10)) +
+  scale_fill_manual(values=c("#43884E","#E8F2E1", "#AAAAAA")) +
+  facet_wrap(~ID, scales="free") +
+  stat_compare_means(
+    comparisons = list(c("Furrow", "Lobe")),
+    method = "wilcox.test",
+    label = "p.format",
+    p.adjust.method = "bonferroni"
+  )+
+  theme_classic(base_size=22)
+
                      hide.ns = TRUE)+
   theme_classic()+
   theme(text = element_text(size=22))
