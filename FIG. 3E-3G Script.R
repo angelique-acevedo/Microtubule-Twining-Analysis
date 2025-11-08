f24df564b3f9dc4d44724fed07a378283e8df18d
Angelique Acevedo <liqacevedo@gmail.com>
2025-11-07 19:31:28 -0600

    Create FIG. 3E-3G Script.R

--- FIG. 3E-3G Script.R
+++ FIG. 3E-3G Script.R
@@ -8,7 +8,7 @@ library(RColorBrewer)
 library (ggpattern)
 
 #2. Load in dataset and remove NAs from data. Datasets for this graph include read.csv("~/Downloads/[Hypocotyl,IN1,IN3,IN6]_Angle_Stages.csv", stringsAsFactors=TRUE)
-datum<- read.csv("~/Downloads/Hypocotyl_Angle_Stages.csv", stringsAsFactors=TRUE)
+datum<- read.csv("~/Downloads/IN6_Angle_Stages.csv", stringsAsFactors=TRUE)
 datum <- na.omit(datum)
 
 #3. Correct the angle column: Add 180 to negative angles to transform data to be continuous from 0 to 180 degrees
@@ -23,7 +23,7 @@ datum$Angle_Category <- cut(
 )
 
 #5. Filter for Stage 2
-stage2_data <- datum %>% filter(Stage == "S2")
+stage2_data <- datum %>% filter(Stage == "S5")
 
 #6. Get the count of each angle category for Stage 2
 stage2_counts <- table(stage2_data$Angle_Category)
@@ -38,30 +38,34 @@ colr <- c("#8AB0D0", "#F2B770", "#EB8777", "#B7D876") #Main background color of 
 
 p <- ggplot(stage2_df, aes(x = factor(Var1,
                                       levels = c("Transverse",
-                                                 "Right-skewed",
-                                                 "Left-skewed",
-                                                 "Longitudinal")),
+                                                 "Right-skewed","Longitudinal",
+                                                 "Left-skewed")),
                            y = Percentage, fill=Var1)) +
   geom_bar(stat = "identity", color = "black") +  
   geom_text(aes(label = paste0(round(Percentage, 1), "%")),
             vjust = -0.5, size = 6) +  # Percentage labels slightly smaller
   labs(
-    title = "Internode 6 Angle Distribution for Stage 3",
+    title = "Internode6 Angle Distribution for Stage 5",
     x = "Angle Category",
     y = "% of Cells"
   ) +
-#ADD STRIPED TO BARPLOTS
- geom_col_pattern(aes(pattern_angle=Var1, color=Var1, fill=Var1),
+  #ADD STRIPED TO BARPLOTS
+  geom_col_pattern(aes(pattern_angle=Var1, color=Var1, fill=Var1),
                    pattern_fill="white", pattern_color="white", pattern_spacing=.05) +
   scale_color_manual(values = colr) +
   scale_fill_manual(values = colr) +
   scale_pattern_angle_manual(values = c(0, 45, 90, -45)) +
-  
+  scale_x_discrete(labels = c(
+    "Transverse" = "T",
+    "Right-skewed" = "R-H",
+    "Longitudinal" = "Lo.",
+    "Left-skewed" = "L-H"
+  ))+
   theme_classic(base_size = 20) +  # Set base font size to 6
   theme(
     axis.title.x = element_text(size = 20),
     axis.title.y = element_text(size = 20),
-    axis.text.x = element_text(angle = -20,hjust = .5, vjust=0, size = 16),
+    #axis.text.x = element_text(angle = -20,hjust = .5, vjust=0, size = 16),
     axis.text.y = element_text(size = 20),
     plot.title = element_text(size = 20, hjust = 0.5)
   )+
